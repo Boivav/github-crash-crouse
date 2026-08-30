94c65021f1ede6c62f17239c742cb3df4bc06706
Boivav Hazarika <hboivav@gmail.com>
2026-08-30 11:21:45 +0530

    Promise constructor

--- projectRealEstate/prac.js
+++ projectRealEstate/prac.js
@@ -1,10 +1,10 @@
-const numbers = [1,2,3,4,5]
+const promise = new Promise((resolve,reject) => {
+      const result = Math.random() > 0.5
+      if(result){
+        resolve(`Operation successfull`)
+      }
+      else{
+        reject(`Operation unsuccessfull`)
+      }
+})
 
-const product = numbers.reduce((pd,num) => pd * num,1)
-console.log(product)
-console.log(`I love coding.`)
-console.log('The reason to love coding is that it makes you a problem solver and another thing is to get addicted in building things which we think the world needs.')
-console.log(`I love playing tennis.`)
-
-console.log(`I love playing piano`)
-console.log(`I love Math`)
