50db7ba0aab14137724aedce80cbf0494c832892
Boivav Hazarika <hboivav@gmail.com>
2026-08-31 20:01:51 +0530

    Callback hell reduction using promises

--- projectRealEstate/prac.js
+++ projectRealEstate/prac.js
@@ -1,24 +1,27 @@
-function preLoadImg(url){
+function uploadFile(){
     return new Promise((resolve,reject) => {
-          let img = new Image()
-          img.src = url
-          img.alt = 'Image generated using constructed'
-          img.addEventListener('load', () => resolve(img))
-          img.addEventListener('error', () => reject(new Error(`Failed to load`)))
+        console.log(`Step 1: Uploading file`)
+        setTimeout(() => {
+            resolve()
+        },1000)
     })
 }
-async function main(){
-      try{
-        const result = await preLoadImg('https://imgs.search.brave.com/CKFEdELwFyQrkB2w22jA9ifpsJJ2-oEKnuD0jO__IaE/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93YWxs/cGFwZXJzLmNvbS9p/bWFnZXMvdGh1bWJu/YWlsL3NuYXBwZWQt/c3BpZGVyLW1hbi00/ay1tYXJ2ZWwtaXBo/b25lLWNybXZlYXRw/ZTh1ZjVwdHAuanBn')
-        console.log(result)
-        document.getElementById('Image-1').appendChild(result)
-        }catch(error){
-            console.log(error)
-        }
-        finally{
-            console.log(`Work done`)
-        }
+
+function processFile(){
+    return new Promise((resolve,reject) => {
+        console.log(`Step 2: Processing file`)
+        setTimeout(() => {
+            resolve()
+        },1000)
+    })
 }
 
-main()
+function notifyUser(){
+    return new Promise((resolve,reject)=> {
+        console.log(`Step 3: Notifying the User`)
+        setTimeout(() => {
+            resolve()
+        },1000)
+    })
+}
 
