c1cd80d65f2e2d6003f4a0d81194f191b4f0421a
Boivav Hazarika <hboivav@gmail.com>
2026-08-31 20:35:11 +0530

    Main function added

--- projectRealEstate/prac.js
+++ projectRealEstate/prac.js
@@ -1,43 +1,24 @@
-function uploadFile(){
+function createPromise(){
     return new Promise((resolve,reject) => {
-        console.log(`Step 1: Uploading file`)
-        setTimeout(() => {
-            resolve()
-        },1000)
-    })
-}
-
-function processFile(){
-    return new Promise((resolve,reject) => {
-        console.log(`Step 2: Processing file`)
-        setTimeout(() => {
-            resolve()
-        },1000)
-    })
-}
-
-function notifyUser(){
-    return new Promise((resolve,reject)=> {
-        console.log(`Step 3: Notifying the User`)
-        setTimeout(() => {
-            resolve()
-        },1000)
+        let num = Math.random()>0.5
+        if(num){
+            resolve(`Operation successfull`)
+        }
+        else{
+            reject(`Operation unsuccessfull`)
+        }
     })
 }
 
 async function main(){
     try{
-       const result1 = await uploadFile()
-       const result2 = await processFile()
-       const result3 = await notifyUser()
-       console.log(result1)
-       console.log(result2)
-       console.log(result3)
+        const promise1 = createPromise()
+        const promise2 = createPromise()
+        const promise3 = createPromise()
+        const result = await Promise.all
+        console.log(result)
     }catch(error){
         console.log(error)
-    }finally{
-        console.log(`All steps completed.`)
     }
 }
 
-main()
