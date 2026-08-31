515cc6f7741f339e4526b5e810811a3ff0305d90
Boivav Hazarika <hboivav@gmail.com>
2026-08-31 21:28:43 +0530

    Wrote a code where array of URLs were being used

--- projectRealEstate/prac.js
+++ projectRealEstate/prac.js
@@ -1,24 +1,32 @@
-function createPromise(){
+function getImagePromise(url){
     return new Promise((resolve,reject) => {
-        let num = Math.random()>0.5
-        if(num){
-            resolve(`Operation successfull`)
-        }
-        else{
-            reject(`Operation unsuccessfull`)
-        }
+        setTimeout(() => {
+            const img = new Image()
+            img.src = url
+            img.alt = `Image generated making use of promises of Javascript concepts`
+            img.addEventListener('load',() => resolve(img))
+            img.addEventListener('error',() => reject(new Error(`Failed to load images: ${url}`)))
+        },500)
     })
 }
 
-async function main(){
+const images = [
+      'https://wallpapercave.com/wp/wp7477462.jpg',
+      'https://wallpapercave.com/wp/wp7477462.jpg',
+      'https://wallpapercave.com/wp/wp7477462.jpg'
+]
+async function preloadImages(imageUrlsArr){
+    const imageContainer = document.getElementById('imgContainer')
+    const uploadContainer = document.getElementById('uploadContainer')
+    const promises = imageUrlsArr.map((url) => getImagePromise(url))
     try{
-        const promise1 = createPromise()
-        const promise2 = createPromise()
-        const promise3 = createPromise()
-        const result = await Promise.all
-        console.log(result)
+        const result = await Promise.all(promises)
+        console.log('All images uploaded successfully')
+        uploadContainer.style.display = 'none'
+        result.forEach((img) => imageContainer.appendChild(img) )
     }catch(error){
         console.log(error)
     }
 }
 
+preloadImages(images)
